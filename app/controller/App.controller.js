7f1e03a1039b1265339fcf20becb2111d794c2a8
necavit <davmarod@gmail.com>
2019-01-08 18:29:16 +0100

    added data binding through the use of a jsonmodel. added an sapui5 input control to the view to test binding the model to the view, from the controller

--- app/controller/App.controller.js
+++ app/controller/App.controller.js
@@ -1,8 +1,17 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+  "sap/m/MessageToast",
+  "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
   return Controller.extend("app.controller.App", {
+    onInit : function () {
+      var data = {
+         recipient : {
+            name : "World"
+         }
+      };
+      this.getView().setModel(new JSONModel(data));
+    },
     onSayHi : function() {
       MessageToast.show("Hi, toast!");
     }
