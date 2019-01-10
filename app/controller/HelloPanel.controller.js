ed5e7a6a227eff7668638ce27fdf97a13f3c2b04
necavit <davmarod@gmail.com>
2019-01-10 12:42:58 +0100

    added a fragment containing a dialog, which is launched from a button click. The fragment is instantiated on demand, using a javascript promise and then cached using an id, within the view. The fragment is also tied to the lifecycle of the containing view, so that it is destroyed and resources released together with view destruction

--- app/controller/HelloPanel.controller.js
+++ app/controller/HelloPanel.controller.js
@@ -1,13 +1,31 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+  "sap/m/MessageToast",
+  "sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
   return Controller.extend("app.controller.HelloPanel", {
     onSayHi : function() {
       var bundle = this.getView().getModel("i18n").getResourceBundle();
       var recipient = this.getView().getModel().getProperty("/recipient/name");
       var message = bundle.getText("inputDescriptionHelloRecipient", [recipient])
       MessageToast.show(message);
+    },
+
+    onShowDialog : function() {
+      var view = this.getView();
+      var dialogId = "helloDialog"
+      var dialog = view.byId(dialogId)
+      if (!dialog) {
+        Fragment.load({
+          name : "app.fragment.DialogHello",
+          id : view.getId()
+        }).then(function(resolvedDialog) {
+          dialog = resolvedDialog;
+          view.addDependent(dialog);
+          dialog.open();
+        });
+      }
+      dialog.open();
     }
   });
 });
