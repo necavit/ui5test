9c52bf002fbf16460dd1a8e18ba2ffb13fe6cf19
necavit <davmarod@gmail.com>
2019-01-10 11:37:36 +0100

    refactored a panel to be included from the containing app and page layout

---
+++ app/controller/HelloPanel.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+  return Controller.extend("app.controller.HelloPanel", {
+    onSayHi : function() {
+      var bundle = this.getView().getModel("i18n").getResourceBundle();
+      var recipient = this.getView().getModel().getProperty("/recipient/name");
+      var message = bundle.getText("inputDescriptionHelloRecipient", [recipient])
+      MessageToast.show(message);
+    }
+  });
+});
